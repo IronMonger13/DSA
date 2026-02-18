06bd174c77bad6a9c7746c87dfbf7683be16fb92
IronMonger13 <ishaanbhalla632@gmail.com>
2026-02-18 10:33:33 +0530

    Modified structure template from .txt to .py for easier templating

---
+++ structure_template.py
@@ -0,0 +1,36 @@
+"""
+INTUITION:
+Explain the problem in simple language.
+
+
+APPROACH:
+Step-by-step logic of how the problem is solved.
+
+
+EDGE CASES:
+Important boundary conditions
+"""
+
+# CODE SOLUTION:
+# The actual working solution.
+
+
+"""
+TIME COMPLEXITY:
+What and why?
+
+
+SPACE COMPLEXITY:
+What and why?
+
+
+WHY BRUTE FORCE FAILS:
+Why brute force is inefficient, or if brute force is acceptable.
+
+
+WHAT I'D SAY IN AN INTERVIEW:
+Explain in 3 points:
+1. What the problem is asking.
+2. The core idea / logic of the solution.
+3. Time and space complexity.
+"""
